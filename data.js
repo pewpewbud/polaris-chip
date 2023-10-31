40bf23d560125fc2fe1200418fcbf0c26dd7e378
pewp <117838302+pewpewbud@users.noreply.github.com>
2023-10-31 17:51:16 -0400

    test

---
+++ data.js
@@ -0,0 +1,53 @@
+const cardData = [
+    {
+      tile: 'white-on-blue',
+      name: '88,000+ students',
+    },
+    {
+      tile: 'navy-on-white',
+      name: '58%',
+      additionalText: 'Of students are Pennsylvania residents.',
+      multiline: true,
+    },
+    {
+      tile: 'white-on-gradient-blue',
+      name: 'Students from 50 states and 145 countries',
+    },
+    {
+      tile: 'white-on-picture',
+      name: 'Check out Undergraduate Admissionss statistics',
+      backgroundImage: 'https://www.psu.edu/psu-edu-assets/images/power-facts/penn-state-tuition.jpg',
+      link: 'https://admissions.psu.edu/apply/statistics/',
+      clickable: true,
+      buttonbottom: true,
+    },
+    {
+      tile: 'white-on-blue',
+      name: 'Nearly 10,000 international students',
+    },
+    {
+      tile: 'navy-on-white',
+      name: '39%',
+      additionalText: 'Of students are from diverse backgrounds.',
+      multiline: true,
+    },
+    {
+      tile: 'white-on-navy',
+      name: '7,900+ faculty across all campuses',
+    },
+    {
+      tile: 'white-on-navy',
+      name: '759,000+ alumni',
+    },
+    {
+      tile: 'white-on-picture',
+      name: 'See Penn State\'s Data Digest dashboards',
+      backgroundImage: 'https://www.psu.edu/psu-edu-assets/images/power-facts/data-digest.jpg',
+      link: 'https://datadigest.psu.edu/',
+      clickable: true,
+      buttonbottom: true,
+    },
+  ];
+  
+  export { cardData };
+  
