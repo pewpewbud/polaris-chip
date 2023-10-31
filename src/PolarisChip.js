a8ce55c54af29ff9a0c6796d5a45efce0b6a6ab5
pewp <117838302+pewpewbud@users.noreply.github.com>
2023-10-31 19:29:25 -0400

    Update PolarisChip.js

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -10,7 +10,7 @@ export class PolarisChip extends LitElement {
       clickable: { type: Boolean },
       multiline: { type: Boolean, reflect: true },
       additionalText: { type: String },
-      buttonBottom: { type: Boolean },
+      buttonbottom: { type: Boolean }
     };
   }
 
@@ -19,16 +19,16 @@ export class PolarisChip extends LitElement {
       :host {
         display: inline-block;
       }
-      :host([buttonBottom]) .button {
+
+      :host([buttonbottom]) .button {
         position: absolute;
-        bottom: 15px;
-        right: 15px;
-        width: 24px;
-        height: 24px;
+        bottom: 14px;
+        right: 14px;
+        width: 23px;
+        height: 23px;
         background-image: url('https://static-00.iconduck.com/assets.00/external-link-icon-2048x2048-wo7lfgrz.png');
         background-size: cover;
         transition: transform 0.2s;
-        color: white;
       }
 
       .tile.white-on-picture:hover .button {
@@ -51,16 +51,19 @@ export class PolarisChip extends LitElement {
         box-shadow: rgba(0, 3, 33, 0.1) 0px 8px 16px 0px;
         position: relative;
       }
+
       .splitspacer {
         width: 242px;
         margin: auto;
       }
+
       .split-line {
         margin: 0 0 16px 0;
         border: 16px;
         height: 3px;
         background-color: rgb(0, 30, 68);
       }
+
       .additionalText {
         font-family: Tahoma;
         display: block;
@@ -71,28 +74,34 @@ export class PolarisChip extends LitElement {
         border: 0;
         padding: 0 47px;
       }
+
       .name {
         margin: 0 0 16px 0;
         border: 0;
         padding: 0 47px;
         margin-bottom: 18px;
       }
+
       .white-on-navy {
         background-color: rgb(0, 30, 68);
       }
+
       .white-on-blue {
         background-color: rgb(30, 64, 124);
       }
+
       .white-on-gradient-blue {
         background-color: rgba(0, 0, 0, 0);
         background-image: linear-gradient(rgb(30, 64, 124) 0%, rgb(0, 30, 68) 65%, rgb(0, 30, 68) 100%);
       }
+
       .white-on-picture {
         background-blend-mode: multiply;
         background-color: rgba(0, 3, 33, 0.5);
         background-position: 50% 50%;
         background-size: cover;
       }
+
       .navy-on-white {
         background-color: rgb(255, 255, 255);
         color: rgb(0, 30, 68);
@@ -102,11 +111,11 @@ export class PolarisChip extends LitElement {
 
   constructor() {
     super();
-    this.name = "default";
+    this.name = 'default';
     this.backgroundImage = '';
     this.link = '';
     this.clickable = false;
-    this.additionalText = "default";
+    this.additionalText = 'default';
     this.multiline = false;
   }
 
@@ -124,35 +133,33 @@ export class PolarisChip extends LitElement {
 
   render() {
     return html`
-      <div class="tile ${this.tile} ${this.multiline ? '' : this.clickable ? 'clickable' : ''}">
+      <div class="tile ${this.tile} ${this.multiline ? 'multiline' : ''} ${this.clickable ? 'clickable' : ''}">
         ${this.multiline
           ? html`
-            <div class="content">
+              <div class="content">
+                <div class="name">
+                  ${this.name}
+                </div>
+                <div class="splitspacer">
+                  <hr class="split-line" />
+                </div>
+                <div class="additionalText">
+                  ${this.additionalText}
+                </div>
+              </div>
+            `
+          : html`
               <div class="name">
                 ${this.name}
               </div>
-              <div class="splitspacer">
-                <hr class="split-line" />
-              </div>
-              <div class="additionalText">
-                ${this.additionalText}
-              </div>
-            </div>`
-          : html`
-            <div class="name">
-              ${this.name}
-            </div>
-            <div class="button"></div>`
-        }
-        ${this.tile === 'white-on-picture' ? html`
-          <style>
-            .tile.white-on-picture {
-              background-image: url(${this.backgroundImage});
-            }
-          </style>` : ''}
+              <div class="button"></div>
+              <style>
+                .tile.white-on-picture {
+                  background-image: url(${this.backgroundImage});
+                }
+              </style>
+            `}
       </div>
     `;
   }
 }
-
-customElements.define('polaris-chip', PolarisChip);
